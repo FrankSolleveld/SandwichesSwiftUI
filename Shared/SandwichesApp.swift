ea2e8e64a1ae32f9decd0ab5f5ec693b37dbf31d
IAmMisterFrank <fsolleveld@outlook.com>
2020-07-14 17:04:12 +0200

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Frank Solleveld on 14/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
